d5495dd37c7884824b205f70938a13c541cbdcf7
Colter <colterholmes@gmail.com>
2025-08-27 21:48:57 -0500

    removed image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,14 +21,6 @@ export const metadata: Metadata = {
     type: "website",
     siteName: "Hack OKState",
     locale: "en_US",
-    images: [
-      {
-        url: "/og-image.png",
-        width: 1200,
-        height: 630,
-        alt: "Hack OKState '25 - Oklahoma State University Hackathon",
-      },
-    ],
   },
   twitter: {
     card: "summary_large_image",
